b41f2e4c5812c15fd9134f3cc8cd5b4ba7e48b3e
caiodotmm <caiomm@email.com>
2025-06-09 09:28:25 -0300

    Definicao dos modelos

---
+++ app/models/stock.py
@@ -0,0 +1,8 @@
+class Stock:
+    def __init__(self, id, name, description, status, creation_date, update_date):
+        self.id = id
+        self.name = name 
+        self.description = description
+        self.status = status
+        self.creation_date = creation_date
+        self.update_date = update_date
